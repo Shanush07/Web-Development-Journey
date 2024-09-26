2cc8c1f5cb1026272683fc912f78e128d860714e
Shanush <131841973+Shanush07@users.noreply.github.com>
2024-09-26 06:41:11 +0530

    Add files via upload

---
+++ NS-2-DAY 1/math.js
@@ -0,0 +1,31 @@
+const sum = (a,b)=> a+b;
+const mul = (a,b)=> a*b;
+const g = 9.8;
+const pi = 3.14;
+
+// module.exports.sum = (a,b)=> a+b;
+// module.exports.mul = (a,b)=> a*b;
+// module.exports.g = 9.8;
+// module.exports.pi = 3.14;
+
+//exports.sum = (a,b)=> a+b;
+//exports.mul = (a,b)=> a*b;
+//exports.g = 9.8;
+//exports.pi = 3.14;
+
+// module.exports = 768;
+
+let obj = {
+    sum: sum,
+    mul: mul,
+    g: g,
+    pi: pi,
+};
+
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     pi: pi,
+// };
+
